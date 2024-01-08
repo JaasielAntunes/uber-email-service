35d4adc8f708296e6d1fcdb94038ecbf883c8680
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-01-08 11:53:26 -0300

    Classe para envio de email criada

--- src/main/java/com/antunes/uberemailservice/application/EmailSenderService.java
+++ src/main/java/com/antunes/uberemailservice/application/EmailSenderService.java
@@ -2,11 +2,15 @@ package com.antunes.uberemailservice.application;
 
 import com.antunes.uberemailservice.adapters.EmailSenderGateway;
 import com.antunes.uberemailservice.core.EmailSenderUseCase;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
 
+@Service
 public class EmailSenderService implements EmailSenderUseCase {
 
     private final EmailSenderGateway emailSenderGateway;
 
+    @Autowired
     public EmailSenderService(EmailSenderGateway emailGateway) {
         this.emailSenderGateway = emailGateway;
     }
