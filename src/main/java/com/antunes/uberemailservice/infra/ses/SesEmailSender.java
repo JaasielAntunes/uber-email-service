4eb85654086d67c9f316cc462974706526dc4e5b
Jaasiel <69393570+JaasielAntunes@users.noreply.github.com>
2024-01-08 11:59:23 -0300

    Criando exceção em caso de falha ao enviar o email.

--- src/main/java/com/antunes/uberemailservice/infra/ses/SesEmailSender.java
+++ src/main/java/com/antunes/uberemailservice/infra/ses/SesEmailSender.java
@@ -4,6 +4,7 @@ import com.amazonaws.AmazonServiceException;
 import com.amazonaws.services.simpleemail.AmazonSimpleEmailService;
 import com.amazonaws.services.simpleemail.model.*;
 import com.antunes.uberemailservice.adapters.EmailSenderGateway;
+import com.antunes.uberemailservice.core.exceptions.EmailServiceException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -29,7 +30,7 @@ public class SesEmailSender implements EmailSenderGateway {
         try {
             this.amazonSimpleEmailService.sendEmail(request);
         } catch (AmazonServiceException e) {
-            throw  new EmailServiceException("Falha ao enviar o email!");
+            throw  new EmailServiceException("Falha ao enviar o email!", e);
         }
     }
 }
